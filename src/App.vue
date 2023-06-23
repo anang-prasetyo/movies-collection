1068bfb84c3c1a3423eaaa5a57fbab04111b23a6
L200180063 <anprasetyo060@gmail.com>
2023-06-23 21:37:34 +0700

    modify almost entire code to make it responsive

--- src/App.vue
+++ src/App.vue
@@ -69,6 +69,8 @@ const showSlides = async() => {
 const isFilm = ref(true)
 const isSeries = ref(true)
 const tagMovie = ref('semua')
+const isKosong = ref(false)
+const isFilmOrSeries = ref(null)
 
 watch(
   () => {
@@ -77,21 +79,32 @@ watch(
       for (let a= 0; a<movies.value.length ; a++){
         newMovies.value.push(movies.value[a])
       }
+      isKosong.value = false
     }
     if (!isSeries.value && isFilm.value){
+      isFilmOrSeries.value = 'Film'
+      isKosong.value = false
       newMovies.value = []
       for (let a= 0; a<movies.value.length ; a++){
         if (movies.value[a].series == 0){
           newMovies.value.push(movies.value[a])
         }
       }
+      if (tagMovie.value !== 'semua' &&!newMovies.value[0].tag.includes(tagMovie.value)){
+        isKosong.value = true
+      }
     }
     if (!isFilm.value && isSeries.value){
+      isFilmOrSeries.value = 'Series'
+      isKosong.value = false
       newMovies.value = []
       for (let a= 0; a<movies.value.length ; a++){
         if (movies.value[a].series !== 0){
           newMovies.value.push(movies.value[a])
         }
+      }
+      if (tagMovie.value !== 'semua' && !newMovies.value[0].tag.includes(tagMovie.value)){
+        isKosong.value = true
       }
     }
   }
@@ -151,9 +164,9 @@ onMounted(() => {
 <template>
   <main>
     <div class="bg-poster position-relative">
-      <div class="nav-logo position-absolute top-0 w-100">
-        <div class="title text-danger text-logo">
-          NETPLIK
+      <div class="px-4 py-3 px-md-5 position-absolute top-0 w-100">
+        <div class="display-4 text-danger text-logo">
+          netplik
         </div>
       </div>
       <div class="vh-100 d-flex align-items-center justify-content-center">
@@ -164,10 +177,10 @@ onMounted(() => {
     </div>
     <span id="TopContent"></span>
     <div class="position-sticky z-2 top-0" style="backdrop-filter: blur(16px) saturate(180%);
-    -webkit-backdrop-filter: blur(16px) saturate(180%); background-color: rgba(17, 25, 40, 0.75); min-height: 320px; max-height: 320px;">
-      <div class="container mb-6 py-2">
-        <div class="row d-flex gap-3 align-items-center justify-content-center position-relative">
-          <div class="col-auto d-flex align-items-center">
+    -webkit-backdrop-filter: blur(16px) saturate(180%); background-color: rgba(17, 25, 40, 0.75);">
+      <div class="container-fluid container-md mb-6 py-2 d-flex flex-column justify-content-between align-items-center h-100">
+        <div class="row d-flex gap-3 align-items-center justify-content-center position-relative h-100 w-100">
+          <div class="col-auto">
             <div v-if="selectedMovie && showDetailMovie" style="max-width: 400px; max-height: 250px;">
               <iframe v-if="selectedMovie.series !== 0" width="400" height="250" :src="selectedMovie.videoUrl[selectedMovie.selectSeries-1]" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
               <iframe v-else width="400" height="250" :src="selectedMovie.videoUrl" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
@@ -184,20 +197,20 @@ onMounted(() => {
               <span class="toast-my--progress"></span>
             </div>
           </div>
-          <div v-if="selectedMovie && selectedMovie.series !== 0" class="col-auto" style="max-height: 250px; overflow: auto;">
-            <div v-for="s in selectedMovie.series" :key="s" class=" d-flex flex-column gap-1">
+          <div v-if="selectedMovie && selectedMovie.series !== 0" class="col-auto d-flex flex-sm-column align-items-center eps">
+            <div v-for="s in selectedMovie.series" :key="s" style="white-space: nowrap;">
               <div :class="selectedMovie.selectSeries == s ? 'text-white' : 'text-white-50'" class="btn btn-default" @click="selectedMovie.selectSeries = s, isClikedMovie = true">Eps {{ s }}</div>
             </div>
           </div>
-          <div v-if="selectedMovie" class="col-4 fs-2">{{ selectedMovie.judul + ' (' + selectedMovie.tahun + ')' }}</div>
-          <div v-else class="col-4 fs-2">Mau nonton apa hari ini?</div>
+          <div v-if="selectedMovie" class="col-auto col-md-4 px-4 px-md-0 fs-md-2 text-sm-center">{{ selectedMovie.judul + ' (' + selectedMovie.tahun + ')' }}</div>
+          <div v-else class="col-auto col-md-4 text-center fs-md-2">Mau nonton apa hari ini?</div>
           <div class="position-absolute bottom-0 start-0 flex-column w-auto" :class="iSFilter ? 'd-inline-flex' : 'd-none'" style="cursor:pointer;">
             <div :class="selectFilter === 'Judul' ? 'text-white' : 'text-white-50'" @click="selectFilter = 'Judul', stateFilter[1] = 0, stateFilter[0] < 2 ? stateFilter[0]++ : stateFilter[0] == 2 ? stateFilter[0]-- : '', stateFilter[0] == 1 ? newMovies.sort(filterDownJudul) : stateFilter[0] == 2 ? newMovies.sort(filterUpJudul) : ''"><i :class="stateFilter[0] == 2 ? 'bi-arrow-up-short' : stateFilter[0] == 1 ? 'bi-arrow-down-short' : 'bi-dash'"></i> Judul</div>
             <div :class="selectFilter === 'Tahun' ? 'text-white' : 'text-white-50'" @click="selectFilter = 'Tahun', stateFilter[0] = 0, stateFilter[1] < 2 ? stateFilter[1]++ : stateFilter[1] == 2 ? stateFilter[1]-- : '', stateFilter[1] == 1 ? newMovies.sort(filterDownTahun) : stateFilter[1] == 2 ? newMovies.sort(filterUpTahun) : ''"><i :class="stateFilter[1] == 2 ? 'bi-arrow-up-short' : stateFilter[1] == 1 ? 'bi-arrow-down-short' : 'bi-dash'"></i> Tahun</div>
           </div>
         </div>
-        <nav class="my-2">
-          <ul class="nav">
+        <nav class="mt-1 mx-auto mx-md-2 d-flex align-items-center row">
+          <ul class="nav col-auto">
             <li class="nav-item"><i class="nav-link" :class="iSFilter ? 'bi-filter-circle-fill' : 'bi-filter-circle'" style="cursor: pointer;" @click="iSFilter = !iSFilter"></i></li>
             <li class="nav-item" @click="tagMovie = 'semua', isFilm = true, isSeries = true, resetFilter()">
               <a href="#TopContent" class="nav-link " :class="isFilm && isSeries ? 'text-white' : 'text-white-50'">Semua</a>
@@ -209,6 +222,8 @@ onMounted(() => {
               <a class="nav-link" :class="isSeries ? 'text-success' : 'text-white-50'">Series</a>
             </li>
             <li class="nav-link" :class="tagMovie !== 'semua' ? 'text-white' : 'text-white-50'" style="cursor:default;"><i class="bi bi-arrow-right-short"></i></li>
+          </ul>
+          <ul class="nav d-inline-flex col" style="overflow: auto; max-height: 40px;">
             <li v-for="t, i in tags.sort()" :key="i" class="nav-item" @click="tagMovie = t">
               <a href="#TopContent" class="nav-link" :class="tagMovie == t ? 'text-white' : 'text-white-50'">{{ t }}</a>
             </li>
@@ -217,9 +232,10 @@ onMounted(() => {
       </div>
     </div>
 
+    <div v-if="isKosong" class="container">Tidak ada {{ isFilmOrSeries }} pada kategori {{ tagMovie }}</div>
     <div v-if="tagMovie == 'semua'" class="container pt-2 pb-3 mt-3">
-      <div v-if="tagMovie == 'semua'" class="row" style="row-gap: 3rem;">
-        <div v-for="m, i in newMovies" :key="i" class="col-2">
+      <div class="row row-gap-4">
+        <div v-for="m, i in newMovies" :key="i" class="m-auto m-sm-0 col-6 col-sm-4 col-md-3 col-lg-2">
           <div style="cursor: pointer;">
             <div class="poster" @click="selectedMovie = m, selectedMovie.selectSeries = 1, isClikedMovie = true">
               <img :src="m.posterUrl" :alt="m.judul" />
@@ -232,8 +248,8 @@ onMounted(() => {
     </div>
     <div v-else class="container pt-2 pb-3 mt-3">
       <div class="row" style="row-gap: 3rem;">
-        <div v-for="m, i in newMovies" :key="i"  :class="m.tag.includes(tagMovie) ? 'col-2' : 'd-none'">
-          <div v-if="m.tag.includes(tagMovie)" @click="selectedMovie = m, selectedMovie.selectSeries = 1, isClikedMovie = true" style="cursor: pointer;">
+        <div v-for="m, i in newMovies" :key="i" :class="m.tag.includes(tagMovie) ? 'm-auto m-sm-0 col-8 col-sm-6 col-md-4 col-lg-2' : 'd-none'">
+          <div @click="selectedMovie = m, selectedMovie.selectSeries = 1, isClikedMovie = true" style="cursor: pointer;">
             <div class="poster">
               <img :src="m.posterUrl" :alt="m.judul" />
               <div class="overlay">{{ m.judul + ' (' + m.tahun + ')' }}</div>
@@ -274,4 +290,18 @@ onMounted(() => {
     width: calc(100% - 10px);
   }
 } 
+
+.eps{
+  overflow: auto;
+}
+@media (min-width: 576px) { 
+  .eps{
+    max-height: 250px;
+  }
+}
+@media (max-width: 576px) { 
+  .poster{
+    height: 350px;
+  }
+}
 </style>
